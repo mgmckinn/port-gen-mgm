442703faf692bcbc45ffb7e02e29132d07c6683f
mgmckinn <mgmckinn2325@me.com>
2022-07-12 22:45:49 -0400

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const generatePage = require('../page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Porfolio complete! Check out index.html');
+}
+);
