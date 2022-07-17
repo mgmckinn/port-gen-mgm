aadf6bb14e7e770328e7df5da7f576d84bb1c645
mgmckinn <mgmckinn2325@me.com>
2022-07-16 23:01:52 -0400

    changes to app

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const fs = require('fs');
-const generatePage = require('../page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+/** @format */
 
+const inquirer = require("inquirer");
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Porfolio complete! Check out index.html');
-}
-);
+// const fs = require('fs');
+// const generatePage = require('../page-template.js');
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log('Porfolio complete! Check out index.html');
+// }
+// );
+
+inquirer;
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ]);
+};
+
+promptUser().then((answers) => console.log(answers));
